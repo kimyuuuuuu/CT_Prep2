4f65f0d3c62f162d1c0695b31c6e7bddb11acb08
YuMin Kim <helloaiworld@gmail.com>
2022-09-25 19:18:40 +0900

    complete

--- CT_3.py
+++ CT_3.py
@@ -1,22 +1,25 @@
 def solution(queue1, queue2):
     q = queue1 + queue2
-    num = sum(q) / 2
-    queue1_sum = sum(queue1)
-    count, inputnum, output = 0, len(queue1) - 1, 0
+    num = sum(q) // 2
 
-    while inputnum < len(q) and output < len(q) :
-        if sum(queue1) == num and sum(queue2) == num :
-            return count 
-        
-        elif sum(queue1) < num and inputnum < len(q)-1 :
-            inputnum += 1
-            queue1_sum  += q[inputnum]
-        
-        else :
-            queue1_sum -= q[output]
-            output += 1
+    output1, input1 = 0, len(queue1)-1
+    queue1_sum = sum(queue1)
+    count = 0
+
+    while output1 < len(q) and input1 < len(q):        
+        if queue1_sum == num :
+            return count
+
+        elif queue1_sum < num and input1 < len(q)-1:
+            input1 += 1
+            queue1_sum += q[input1]
+
+        else:
+            queue1_sum -= q[output1]
+            output1 += 1
 
         count += 1
+
     return -1
 
 queue1 = [3, 2, 7, 2]
