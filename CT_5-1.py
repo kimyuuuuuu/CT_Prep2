b5b7b1c426ec4ee570f4f1f1c1ab314faebaef0f
kimyuuuuuu <helloaiworld01@gmail.com>
2022-10-01 12:41:41 +0900

    5-1, 5-3완료

---
+++ CT_5-1.py
@@ -0,0 +1,10 @@
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right+1):
+        answer.append(max((i // n), (i % n)) + 1)
+        
+    return answer
+
+n = int(input())
+left = int(input())
+right = int(input())
---
+++ CT_5-2.py
@@ -0,0 +1,9 @@
+def solution(numbers, hand):
+    answer = ''
+    
+
+
+    return answer
+
+numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+hand = 	"right"
