539daf83d3f92e2f23038891af9b469af51e87bf
kimyuuuuuu <helloaiworld01@gmail.com>
2022-10-01 13:17:02 +0900

    CT_5-2 non numpy

--- CT_5-2.py
+++ CT_5-2.py
@@ -1,6 +1,18 @@
+import numpy as np
+
 def solution(numbers, hand):
     answer = ''
-    
+
+    for i in numbers:
+        if i == 1 or i == 4 or i == 7:
+            answer += L
+        
+        elif i == 3 or i == 6 or i == 9:
+            answer += R
+
+        else :
+            if 
+        
 
 
     return answer
