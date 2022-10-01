9761cbacf0f34cb796a82684197e3f4c54d4f8f1
kimyuuuuuu <helloaiworld01@gmail.com>
2022-10-01 15:52:53 +0900

    CT_4 complete

--- CT_4.py
+++ CT_4.py
@@ -1,4 +1,31 @@
-def solution(n, left, right):
-    answer = []
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people.sort()
+
+    boat = deque(people)
+
+    while boat:
+
+        if len(boat) < 2:
+            if boat[0] <= limit:
+                boat.pop()
+                answer += 1
+        
+        else :
+            if boat[0] + boat[-1] > limit:
+                boat.pop()
+                answer += 1
+
+            else:
+                boat.pop()
+                boat.popleft()
+                answer += 1
+
+
     return answer
 
+people = [70, 50, 80, 50]
+limit = 100
+print(solution(people, limit))
